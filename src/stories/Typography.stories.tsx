47539cad2082a5aba8b028ed3365a9ddc98a9a8b
farhanjamil295 <farhanjamil295@gmail.com>
2022-03-26 23:27:14 +0500

    feat: typography component

---
+++ src/stories/Typography.stories.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+import { Canvas, Story } from "@storybook/addon-docs";
+import { templateForComponent } from "./Helpers";
+
+import CText, { CTextProps } from "../components/typography/CText";
+
+/**
+ * Initialize meta data for component story
+ */
+const meta: Meta = {
+  title: "Components/Typography",
+  component: CText,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+};
+
+/**
+ * Create template to create multiple variants of the component
+ */
+const Template = templateForComponent(CText);
+
+//Button color variants
+export const Text = Template({
+  type: "heading-1",
+  children: "This is a Typography Component",
+});
+
+export const TypographyStories = (): React.ReactElement => {
+  const types: CTextProps["type"][] = [
+    "heading-1",
+    "heading-2",
+    "heading-3",
+    "heading-4",
+    "heading-5",
+    "heading-6",
+    "paragraph",
+  ];
+
+  const colors: CTextProps["color"][] = [
+    "primary",
+    "success",
+    "warning",
+    "danger",
+    "mute",
+    "default",
+  ];
+
+  return (
+    <div>
+      {types.map((t, i) => {
+        return (
+          <CText key={i} type={t}>
+            {t}
+          </CText>
+        );
+      })}
+
+      {colors.map((c, i) => {
+        return (
+          <CText key={i} type="paragraph" color={c}>
+            This is a {c} paragraph
+          </CText>
+        );
+      })}
+    </div>
+  );
+};
+
+TypographyStories.story = {
+  name: "All Variants",
+};
+
+export default meta;
